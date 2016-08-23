7f3fbd5ff3cfe3a2aaba35f0f98269ca63fa2655
emguy <emguy.yuzhang@gmail.com>
2016-08-22 23:04:42 -0400

    Implemented the view and the log for display posts.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import monk from 'monk';
+
+//let db = monk('127.0.0.1/nodeblog', {username: 'emguy', password: '31419'});
+
+var db = require('monk')('emguy:314159@localhost:27017/nodeblog');
+
+//let db = monk('emguy:314159@localhost/nodeblog');
 let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  let db = req.db;
+  let posts = db.get('posts');
+  posts.find({}, {}, (err, posts) => {
+    console.log(err);
+    console.log(posts);
+    res.render('index', { posts: posts });
+  });
 });
 
 export { router as default };
+
